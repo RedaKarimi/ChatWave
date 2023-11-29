f08968f388baf47354bd319022b93dd7bdb6901b
RedaKarimi <redakarimi76@gmail.com>
2023-11-29 22:13:46 +0100

    modified:   App.js 	new file:   App/Home/Home.js 	modified:   package-lock.json 	modified:   package.json

--- App.js
+++ App.js
@@ -1,10 +1,10 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-
+import { Button } from '@rneui/themed';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Button title="Solid" type="solid" loading />
       <StatusBar style="auto" />
     </View>
   );
